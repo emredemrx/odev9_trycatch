1a7145c09bcaaa2b955a94283450d3bd6329d700
emred <femredemir699@gmail.com>
2024-06-26 23:10:18 +0300

    odev eklendi

---
+++ src/main/java/org/example/CustomException.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class CustomException extends Exception{
+    public CustomException(String mesaj) {
+        super(mesaj);
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,20 @@
 package org.example;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        try {
+            int[] numbers = {1, 2, 3, 4, 5};
+            int index = 5;
+            indexSayisi(index);
+            System.out.println(numbers[index]);
+        } catch (CustomException e){
+            System.out.println(e);
+        }
+    }
+    public static void indexSayisi(int number) throws CustomException {
+        if (number < 0) {
+            throw new CustomException("Hata: Negatif sayı girdiniz.");
+        }else if(number > 2){
+            throw new CustomException("index değerimiz dizi eleman sayısından buyuk olamaz");
         }
     }
 }
